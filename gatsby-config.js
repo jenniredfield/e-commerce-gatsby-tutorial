c0e512bcc811831edfef801d2b6acc5bfc8f9c18
Jennifer Bird <jenniredfield@hotmail.com>
2021-02-11 20:03:29 +0000

    Changed background theme colour

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,11 +18,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `fino-do-brecho`,
+        short_name: `fino do brecho`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
